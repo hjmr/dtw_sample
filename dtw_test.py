6d690551071b63adc9a4bfd2034cf48477fa948c
hjmr <2620990+hjmr@users.noreply.github.com>
2021-10-14 23:06:01 +0900

    first working edition.

--- dtw_test.py
+++ dtw_test.py
@@ -6,19 +6,23 @@ import h5py
 
 def parse_arg():
     args = argparse.ArgumentParser(description="Apply DTW.")
-    args.add_argument("-p", "--data_path", type=str, nargs=1, help="A path to data in HDF5.")
     args.add_argument("-r", "--reference", type=str, nargs=1, help="Specify the reference HDF5 file.")
+    args.add_argument("-I", "--input_path", type=str, nargs=1, help="A path to input data.")
     args.add_argument("-o", "--output", type=str, nargs=1, help="Specify the output file.")
-    args.add_argument("-k", "--key", type=str, nargs=1, help="Specify the key to store converted data.")
+    args.add_argument("-O", "--output_path", type=str, nargs=1, help="A path to output data.")
     args.add_argument("FILE", type=str, nargs=1, help="Specify a HDF5 file to convert.")
     return args.parse_args()
 
-def get_data(hdf5_file, data_path):
+def get_data(hdf5_file, input_path):
     data = None
     with h5py.File(hdf5_file, "r") as h5:
-        if data_path in h5.keys():
-            data = np.array(h5[data_path]).T
+        if input_path in h5.keys():
+            data = np.array(h5[input_path]).T
     return data[0]
+
+def put_data(hdf5_file, output_path, data):
+    with h5py.File(hdf5_file, "a") as h5:
+        h5[output_path] = data
 
 def dtw_one(reference, target):
     alignment = dtw.dtw(target, reference)
@@ -27,7 +31,7 @@ def dtw_one(reference, target):
 
 if __name__ == "__main__":
     args = parse_arg()
-    reference = get_data(args.reference[0], args.data_path[0])
-    for f in args.FILES:
-        target = get_data(f, args.data_path[0])
-        converted = dtw_one(reference, target)
+    reference = get_data(args.reference[0], args.input_path[0])
+    target = get_data(args.FILE[0], args.input_path[0])
+    converted = dtw_one(reference, target)
+    put_data(args.output[0], args.output_path[0], converted)
