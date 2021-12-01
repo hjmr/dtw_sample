cec9ea4bac8f4736eccaf324a3b7a144576e2569
hjmr <2620990+hjmr@users.noreply.github.com>
2021-12-01 17:30:27 +0900

    supporting multiple channels

--- conv_to_csv.py
+++ conv_to_csv.py
@@ -11,18 +11,24 @@ DEFAULT_PATH = "98:D3:51:FD:97:15/raw/channel_1"
 
 def parse_arg():
     args = argparse.ArgumentParser(description="Convert to CSV.")
-    args.add_argument("-p", "--path", type=str, help="A data path in HDF5.")
-    args.add_argument("-d", "--dst_dir", type=str, nargs=1, help="A destination directory.")
-    args.add_argument("-s", "--src_dir", type=str, nargs=1, help="A directory contains source files.")
+    args.add_argument("-d", "--device", type=str, default="98:D3:51:FD:97:15", help="Device ID like 98:D3:51:XX:XX:XX")
+    args.add_argument("-c", "--channels", type=int, nargs="+", help="Channes.")
+    args.add_argument("-f", "--from_dir", type=str, nargs=1, help="A destination directory.")
+    args.add_argument("-t", "--to_dir", type=str, nargs=1, help="A directory contains source files.")
     return args.parse_args()
 
 
-def get_data(hdf5_file, data_path):
+def get_data(hdf5_file, device, channels):
     data = None
     with h5py.File(hdf5_file, "r") as h5:
-        if data_path in h5.keys():
-            data = np.array(h5[data_path]).T
-    return data[0]
+        for c in channels:
+            data_path = "{}/raw/channel_{}".format(device, c)
+            if data_path in h5.keys():
+                if data == None:
+                    data = np.array(h5[data_path]).T
+                else:
+                    data = np.append(data, np.array(h5[data_path]).T, axis=0)
+    return data
 
 
 def get_destination(src_file, src_root, dst_root):
@@ -42,10 +48,10 @@ def save_in_CSV(csv_file, data):
     np.savetxt(csv_file, data, delimiter=",", fmt="%d")
 
 
-def to_csv_dir(src_root, dst_root, data_path):
+def to_csv_dir(src_root, dst_root, device, channels):
     src_files = glob.glob("{}/**/*.h5".format(src_root), recursive=True)
     for src_file in src_files:
-        src_data = get_data(src_file, data_path)
+        src_data = get_data(src_file, device, channels)
         dst_dir, dst_filename = get_destination(src_file, src_root, dst_root)
         if not os.path.exists(dst_dir):
             os.makedirs(dst_dir)
@@ -55,5 +61,4 @@ def to_csv_dir(src_root, dst_root, data_path):
 
 if __name__ == "__main__":
     args = parse_arg()
-    data_path = args.path if args.path is not None else DEFAULT_PATH
-    to_csv_dir(args.src_dir[0], args.dst_dir[0], data_path)
+    to_csv_dir(args.from_dir[0], args.to_dir[0], args.device, args.channels)
